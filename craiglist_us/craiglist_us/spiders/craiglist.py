1d13e3bf08199eeab705ec125c6698fe28f2f898
Jithin <cyb3rjithu@gmail.com>
2016-01-23 22:42:08 +0530

    links xpath added

--- craiglist_us/craiglist_us/spiders/craiglist.py
+++ craiglist_us/craiglist_us/spiders/craiglist.py
@@ -41,26 +41,21 @@ class CraiglistSpider(Spider):
         # base xpath for category links and text
         CATEGORIES_BASE_XPATH = '//div[@id="bbb"]/div[@class="cats"]/ul/li/a'
         for link in response.xpath(CATEGORIES_BASE_XPATH):
-            # category_text is passed along with the url as meta_data in
-            # request
-            meta_data = {}
             # category url, need to append base url
             url = link.xpath('./@href').extract()
             if url:
                 # converting the relative url to absolute url
                 url = urljoin(self.base_url, url[0])
-                # catgeory_name
-                url_text = link.xpath('./span/text()').extract()
-                url_text = url_text[0].strip() if url_text else ''
-                meta_data = {'category': url_text}
                 yield Request(url=url,
                               callback=self.parse_links,
-                              meta=meta_data
                               )
 
     def parse_links(self, response):
+        LINKS_XPATH = '//span[@class="rows"]/p[@class="row"]/a/@href'
         NEXT_PAGE_XPATH = '//a[@class="button next"]/@href'
 
+        links = response.xpath(LINKS_XPATH).extract()
+        
         next_page = response.xpath(NEXT_PAGE_XPATH).extract()
         if next_page:
             next_page_url = urljoin(self.base_url, next_page[0])
